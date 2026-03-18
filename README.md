75013d525343ca941caae82c5d69c88f7f3a219f
ravigfortanix <112968441+ravigfortanix@users.noreply.github.com>
2026-03-18 21:36:28 +0530

    Fortanix Azure DevOps Extension Task (#1)
    * Add Fortanix Azure DevOps Extension code
    * Add basic CI for this project
    
    
    ---------
    
    Co-authored-by: YX Cao <yuxiang.cao@fortanix.com>

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# azure-devops-extension-task
+### Description
+
+Azure DevOps is an end-to-end software development platform that offers an assortment of capabilities intended 
+to organize and accelerate development efforts across the entire application lifecycle.
+To keep the secrets secured in an Azure DevOps pipeline, a Secret Management is required. 
+
+### Solution
+
+Leverage Fortanix DSM to securely retrieve the secrets at runtime of Azure DevOps pipeline execution.
+Fortanix offers an ADO custom extension Task to retrieve the secrets.
+
+#### How to build a custom extension in Azure DevOps that fetches the secrets?
+
+1. Install Node js(Latest version is preferred)
+2. Build the extension
+   * Install tfx-cli, command: npm install -g tfx-cli
+   * Configure manifest.env
+   * Run ./build.sh
+        * This will create the files below:
+           * Fortanix-Secret-Management/task.json
+           * Fortanix-Secret-Management/package.json
+           * Fortanix-Secret-Management/node_modules
+           * vss-extension.json
+3. Find the extension in the same directory as <PUBLISHER_ID>.<EXTENSION_NAME>-<VERSION>.vslx
+   * eg: Fortanix-ADO.Secret-Management-1.0.0.vslx
+
+#### manifest.env Attributes
+
+1. MANIFEST_VERSION: Version of manifest
+2. ID: Unique Identifier(UUID), e.g., d9f8b8c2-1b48-4e5c-b0f5-41f2e7cf88a5
+3. EXTENSION_NAME: Name of the extension that is presented while installing this extension
+4. PUBLISHER_ID: ID of the Azure DevOps publisher 
+5. TASK_NAME: Name of the task that is presented in the Azure pipelines
+6. VERSION: Version of the extension
+7. AUTHOR: Name of the Author
+
+#### How to execute the extension
+
+1. Upload the extension to ADO marketplace
+   * https://marketplace.visualstudio.com/manage/publishers/<PUBLISHER_ID>
+2. Share it to the ADO organization
+3. Go to the ADO organization -> Oraganization Settings -> Extensions -> Shared -> Install
+4. Create a new project in ADO organization and provide access to any github repository
+5. Create a new pipeline
+6. Configure FORTANIX_API_KEY as a secret in Variables
+7. Go to show assistant and search for Fortanix-Secret-Management
+8. Configure all the listed paramaters:
+   * FORTANIX_API_ENDPOINT, eg: apac.smartkey.io 
+   * FORTANIX_API_KEY, eg: $(FORTANIX_API_KEY)
+   * FORTANIX_SECRET, name of a Security Object(SECRET)
+   * SECRET_VARIABLE_NAME, variable to save the secret value
+9. This will fetch the secret
+
+#### Pre-requisites
+
+1. To build the extension, Node.js v20 or above is required
+2. FORTANIX DSM Account
+   a. Configure Group, App API key and Security Object(SECRET)
+   b. Ensure to enable the EXPORT permission for Security Object(SECRET)
+3. Azure DevOps Organization
+   a. Configure Publisher, Project, Github repository 
+
+
---
+++ build.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+set -e
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+TASK_DIR="$SCRIPT_DIR/Fortanix-Secret-Management"
+VSS_EXTENSION="$SCRIPT_DIR/vss-extension.json"
+TASK_JSON="$TASK_DIR/task.json"
+handle_error() {
+    echo "ERROR: $1"
+    exit 1
+}
+cleanup() {
+    # cleanup the node_modules and json files
+    echo "cleanup the node_modules and json files"
+    rm -rf "$SCRIPT_DIR/node_modules" "$SCRIPT_DIR/package.json" "$SCRIPT_DIR/package-lock.json" "$VSS_EXTENSION"
+    rm -rf "$TASK_DIR/index.js" "$TASK_JSON"
+}
+trap cleanup EXIT
+# Load environment variables
+set -a
+source "$SCRIPT_DIR/manifest.env"
+set +a
+echo "manifest.env is exported."
+# check if npm is installed
+if command -v npm >/dev/null 2>&1; then
+  echo "npm is already installed at: $(which npm)"
+else
+  handle_error "npm not found. Please install it and try again."
+fi
+# check if tfx-cli is installed
+if command -v tfx > /dev/null 2>&1; then
+  echo "tfx is already installed globally."
+elif npx --no-install tfx-cli --version >/dev/null 2>&1; then
+  echo "tfx is already installed locally."
+else
+  echo "tfx not found — installing it locally"
+  npm install --save-dev "tfx-cli@$TFX_CLI_VERSION"
+  echo "tfx installed successfully."
+fi
+# Check if TypeScript compiler exists
+if npx --no-install tsc --version > /dev/null 2>&1; then
+  echo "tsc is already installed locally"
+else
+  echo "tsc not found — installing it locally"
+  npm install --save-dev "typescript@$TYPE_SCRIPT_VERSION"
+  echo "tsc installed successfully."
+fi
+# Check if Node compiler exists
+if npm list @types/node >/dev/null 2>&1; then
+  echo "types/node is already installed"
+else
+  echo "Installing types/node"
+  npm install --save-dev "@types/node@$TYPE_NODE_VERSION"
+  echo "types/node installed successfully."
+fi
+# Check if azure-pipelines-task-lib is installed
+if npm list azure-pipelines-task-lib >/dev/null 2>&1; then
+  echo "azure-pipelines-task-lib is already installed"
+else
+  echo "Installing azure-pipelines-task-lib"
+  npm install --save-exact "azure-pipelines-task-lib@$AZURE_PIPELINES_VERSION"
+  echo "azure-pipelines-task-lib installed successfully."
+fi
+# check if tsconfig.json exists
+pushd "$TASK_DIR"
+if [ ! -f tsconfig.json ]; then
+  npx tsc --init
+fi
+# Compile the code
+npx tsc
+popd
+echo "Build started"
+# Replace placeholders in the  vss-extension.json
+envsubst < "$SCRIPT_DIR/vss-extension.template.json" > "$VSS_EXTENSION"
+[ -f "$VSS_EXTENSION" ] || handle_error "vss-extension.json was not created."
+echo "vss-extension.json is created."
+# Replace placeholders in the Fortanix-Secret-Management/task.json
+envsubst < "$TASK_DIR/task.template.json" > "$TASK_JSON"
+[ -f "$TASK_JSON" ] || handle_error "task.json was not created."
+echo "$TASK_JSON is created."
+# build the extension
+npx tfx extension create || handle_error "Extension packaging failed."
